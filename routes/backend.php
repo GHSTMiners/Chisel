641102e30aef75c75a604a0c8766991cac2f9911
smokeythebandit <smokeythebandit@users.noreply.github.com>
2021-10-27 23:31:18 +0200

    Fixed route cache issue, and created WalletChallengeController

--- routes/backend.php
+++ routes/backend.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\Backend\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\Backend\HomeController::class, 'index'])->name('administrator');
 
 
 Auth::routes(['register' => true]);
