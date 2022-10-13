4e8d2f8bc77a96481d5874fc0f128bc7d89c5cb7
smokeythebandit <smokeythebandit@users.noreply.github.com>
2022-10-14 00:05:25 +0200

    Scheduled more frequently

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new CalculateServerStats)->everyFifteenMinutes();
+        $schedule->job(new CalculateServerStats)->everyFiveMinutes();
         // $schedule->command('inspire')->hourly();
     }
 
