2e5e198f777609427c6a89208fac1daeb09680c4
smokeythebandit <smokeythebandit@users.noreply.github.com>
2022-10-17 20:30:32 +0200

    sadsa

--- app/Jobs/CalculateServerStats.php
+++ app/Jobs/CalculateServerStats.php
@@ -72,8 +72,8 @@ class CalculateServerStats implements ShouldQueue
             foreach($regions as &$region) {
                 $datapoints[$region->id][-1] = $region->games()->whereBetween('created_at', [$current_date, $next_date])->count();
                 foreach($region->games()->whereBetween('created_at', [$current_date, $next_date])->get() as $game) {
-                    echo $game->uuid;
                     foreach($statistics_categories as $category) {
+                        echo $category->name;
                         echo $game->statistic_entries()->where('game_statistic_category_id', $category->id)->sum('value');
                     }
                 }
