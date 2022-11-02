a2fe0c0c127bc2391ecbb4863b03602da5795fa4
Benjamin Martens <bmartens@pm.me>
2022-11-02 23:17:03 +0100

    Typotje

--- app/Models/GameLogEntry.php
+++ app/Models/GameLogEntry.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class GameStatisticEntry extends Model
+class GameLogEntry extends Model
 {
     use HasFactory;
 
