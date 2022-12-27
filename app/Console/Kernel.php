f90c57d385e0e4dc83041c48d8d6825e0263e5dd
Benjamin Martens <info@1-zero.nl>
2022-12-27 23:10:59 +0100

    Implemented automated backups for Chisel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->job(new CalculateServerStats)->everyFiveMinutes();
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:clean')->daily()->at('01:00');
+        $schedule->command('backup:run')->daily()->at('01:30');
     }
 
     /**
